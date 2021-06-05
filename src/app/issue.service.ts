3e318396eb323f169280496e403096fc4299b791
mikej <mike@mikejenkins.co.uk>
2021-06-05 17:06:09 +0100

    Use environments file to store base URL

--- src/app/issue.service.ts
+++ src/app/issue.service.ts
@@ -1,31 +1,32 @@
-import { Injectable } from "@angular/core";
-import { ITask } from "./task";
+import {Injectable} from "@angular/core";
+import {ITask} from "./task";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from '../environments/environment'
 
 @Injectable({
     providedIn: 'root'
 })
 export class IssueService {
-    private serviceUrl = "http://localhost:8080/task";
+    private baseUrl = environment.baseUrl;
 
     constructor(private http: HttpClient) {
 
     }
 
     getIssue(id: Number): Observable<ITask> {
-        return this.http.get<ITask>(this.serviceUrl + '/' + id);
+        return this.http.get<ITask>(this.baseUrl + '/task/' + id);
     }
 
     getIssues(): Observable<ITask[]> {
-        return this.http.get<ITask[]>(this.serviceUrl + '?pageNo=0&pageSize=15&sortBy=id');
+        return this.http.get<ITask[]>(this.baseUrl + '/task?pageNo=0&pageSize=15&sortBy=id');
     }
 
     postIssue(task: ITask): Observable<any> {
-        return this.http.post(this.serviceUrl, task)
+        return this.http.post(this.baseUrl + '/task', task)
     }
 
     putIssue(id: Number, task: ITask): Observable<any> {
-        return this.http.put(this.serviceUrl + '/' + id, task)
+        return this.http.put(this.baseUrl + '/task/' + id, task)
     }
 }
